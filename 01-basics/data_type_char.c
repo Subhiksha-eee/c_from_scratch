c93210e791bbaa042c009158bcee8f3dd4ef800b
Subhiksha-eee <subhikshasenthilkumar007@gmail.com>
2026-04-08 23:18:16 +0530

    Create data_type_char.c

---
+++ 01-basics/data_type_char.c
@@ -0,0 +1,18 @@
+// size of char
+#include<stdio.h>
+int main(){
+    printf("%d ",sizeof(char));
+    return 0;
+}
+// ASCII 
+#include<stdio.h>
+int main(){
+   char name=65;
+   printf("%c",name);
+}
+// addin -ve sign will not gain any spcl power. every negative will have positive equivalent 
+#include<stdio.h>
+int main(){
+   char name=-128;
+   printf("%c",name);
+}
